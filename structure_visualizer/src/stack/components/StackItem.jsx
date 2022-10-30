88a4a931595ed8dfbca44df2ab1ae74850c05cab
Rodevp <rodriguezrodrigor12@gmail.com>
2022-10-30 11:49:49 -0500

    aplicate animation for stackitem component

--- structure_visualizer/src/stack/components/StackItem.jsx
+++ structure_visualizer/src/stack/components/StackItem.jsx
@@ -1,9 +1,18 @@
-import React from "react";
+import { motion } from "framer-motion";
 
 function StackItem(props) {
   const { item, className } = props;
 
-  return <div className={className}>{item}</div>;
+  return (
+    <motion.div
+      className={className}
+      animate={{ rotate: [0, 3, -3, 0] }}
+      exit={{ opacity: 0 }}
+      transition={{ ease: "easeOut", duration: 1 }}
+    >
+      {item}
+    </motion.div>
+  );
 }
 
 export default StackItem;
